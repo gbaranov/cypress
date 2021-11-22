c0128d951deaaac4f4458f090a3c83ba4d4884c5
Georgy Baranov <floridavbv@gmail.com>
2021-11-21 21:27:24 -0700

    tests

--- cypress/integration/examples/banking_app.spec.js
+++ cypress/integration/examples/banking_app.spec.js
@@ -1,25 +1,39 @@
 describe('Working with inputs', () => {
 
+		it('Override the current time', () => {
+			const date = new Date(2020, 3, 10).getTime()
+			cy.clock(date)
+			cy.log(date)
+		})
+
 		it('Open the login page', () => {
 			cy.visit('http://zero.webappsecurity.com/login.html')
+			cy.clearCookies({log: true})
 		})
 
 		it('Enter the username', () => {
-			cy.get('#user_login').clear();
-			cy.get('#user_login').type('Invalid value')
+			cy.get('#user_login').as('username')
+			cy.get('@username').clear()
+			cy.get('@username').type('Invalid value', {delay: 50})
 		})
 
 		it('Enter the password', () => {
-			cy.get('#user_password').clear();
-			cy.get('#user_password').type('Invalid value')
+			cy.get('#user_password').as('password')
+			cy.get('@password').clear()
+			cy.get('@password').type('Invalid value')
 		})
+
+		it('Mark checkbox', () => {
+			cy.get('input[type="checkbox"]').click()
+			//cy.wait(3000)
+	})
 
 		it('Submit data', () => {
 			cy.contains('Sign in').click()
 		})
 
 		it('Error message appear', () => {
-			cy.get('.alert-error').should('be.visible')
+			cy.get('.alert-error').should('be.visible').and('contain', 'wrong')
 		})
 
 })
---
+++ cypress/integration/examples/device.spec.js
@@ -0,0 +1,3 @@
+describe('Device Test', () => {
+
+})
